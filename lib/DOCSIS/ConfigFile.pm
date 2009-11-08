fd2a9166c9ea99a1ea417cb8f0b30f7e1170d360
Jan Henning Thorsen <git@flodhest.net>
2009-11-08 16:53:00 +0100

    New release: 0.57

--- lib/DOCSIS/ConfigFile.pm
+++ lib/DOCSIS/ConfigFile.pm
@@ -6,7 +6,7 @@ DOCSIS::ConfigFile - Decodes and encodes DOCSIS config-files
 
 =head1 VERSION
 
-Version 0.56
+Version 0.57
 
 =head1 SYNOPSIS
 
@@ -41,7 +41,7 @@ use constant Syminfo => "DOCSIS::ConfigFile::Syminfo";
 use constant Decode  => "DOCSIS::ConfigFile::Decode";
 use constant Encode  => "DOCSIS::ConfigFile::Encode";
 
-our $VERSION = '0.56';
+our $VERSION = '0.57';
 our $TRACE   = 0;
 
 =head1 METHODS

2141f0724468f9dabd3caac304af578482b962ff
Jan Henning Thorsen <jhthorsen@cpan.org>
2010-09-04 11:34:59 +0200

    0.6002   Sat Sep  4 11:34:58 CEST 2010       - Fix dump_symbol_tree() can print large numbers       - Fix dump_symbol_tree() can also follow "vendorspec"       - Fix remove duplicates from Syminfo table

--- lib/DOCSIS/ConfigFile.pm
+++ lib/DOCSIS/ConfigFile.pm
@@ -6,7 +6,7 @@ DOCSIS::ConfigFile - Decodes and encodes DOCSIS config-files
 
 =head1 VERSION
 
-0.0601
+0.6002
 
 =head1 SYNOPSIS
 
@@ -92,7 +92,7 @@ use constant Syminfo => "DOCSIS::ConfigFile::Syminfo";
 use constant Decode  => "DOCSIS::ConfigFile::Decode";
 use constant Encode  => "DOCSIS::ConfigFile::Encode";
 
-our $VERSION = '0.0601';
+our $VERSION = '0.6002';
 our $TRACE   = 0;
 
 =head1 ATTRIBUTES

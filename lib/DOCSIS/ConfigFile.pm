463ba2e2e16f07c82119e46b27cf9b7fc38caf77
Jan Henning Thorsen <jhthorsen@cpan.org>
2010-09-05 20:38:28 +0200

    0.6003   Sun Sep  5 20:38:25 CEST 2010       - Fix uint() cannot return negative integers         Contributor: canni       - Add int() to handle (possible) signed integers         Contributor: canni       - Fix Decode::string() decodes % as %25       - Add no_value() dec/encode function       - Change Decode.pm and Encode.pm to confess on errors       - Change add_symbol() will confess if the symbol already exists       - Change Encode functions return a list (wantarray is history)       - Change examples/docsis.pl is renamed and use JSON instead         of YAML. Might change to Config::Any in the future.

--- lib/DOCSIS/ConfigFile.pm
+++ lib/DOCSIS/ConfigFile.pm
@@ -6,7 +6,7 @@ DOCSIS::ConfigFile - Decodes and encodes DOCSIS config-files
 
 =head1 VERSION
 
-0.6002
+0.6003
 
 =head1 SYNOPSIS
 
@@ -95,7 +95,7 @@ use constant Syminfo => "DOCSIS::ConfigFile::Syminfo";
 use constant Decode  => "DOCSIS::ConfigFile::Decode";
 use constant Encode  => "DOCSIS::ConfigFile::Encode";
 
-our $VERSION = '0.6002';
+our $VERSION = '0.6003';
 our $TRACE   = 0;
 
 =head1 ATTRIBUTES

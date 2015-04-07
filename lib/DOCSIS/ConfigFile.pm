3ab5681826d00d36a4f981492a43b8bec12661da
Jan Henning Thorsen <jhthorsen@cpan.org>
2015-04-07 21:13:49 +0200

    Released version 0.69
           * MtaConfigDelimiter is optional when creating MTA config files
           * Add support for eRouter MangemnetServer TLV #5
             Contributor: Josh Galvez

--- lib/DOCSIS/ConfigFile.pm
+++ lib/DOCSIS/ConfigFile.pm
@@ -6,7 +6,7 @@ DOCSIS::ConfigFile - Decodes and encodes DOCSIS config files
 
 =head1 VERSION
 
-0.68
+0.69
 
 =head1 DESCRIPTION
 
@@ -93,7 +93,7 @@ use constant DEBUG => $ENV{DOCSIS_CONFIGFILE_DEBUG} || 0;
 
 use base 'Exporter';
 
-our $VERSION   = '0.68';
+our $VERSION = '0.69';
 our @EXPORT_OK = qw( decode_docsis encode_docsis );
 our $DEPTH     = 0;
 

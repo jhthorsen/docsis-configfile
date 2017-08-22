a9154336c9cb9afbff11e0a96583df94b4393c49
Jan Henning Thorsen <jhthorsen@cpan.org>
2017-08-22 20:47:26 +0200

    Released version 0.72
     - Add missing Types for TLV 202 #9
     - Will die when pack()-ing invalid length

--- lib/DOCSIS/ConfigFile.pm
+++ lib/DOCSIS/ConfigFile.pm
@@ -6,7 +6,7 @@ DOCSIS::ConfigFile - Decodes and encodes DOCSIS config files
 
 =head1 VERSION
 
-0.71
+0.72
 
 =head1 DESCRIPTION
 
@@ -93,7 +93,7 @@ use constant DEBUG => $ENV{DOCSIS_CONFIGFILE_DEBUG} || 0;
 
 use base 'Exporter';
 
-our $VERSION   = '0.71';
+our $VERSION   = '0.72';
 our @EXPORT_OK = qw( decode_docsis encode_docsis );
 our $DEPTH     = 0;
 
